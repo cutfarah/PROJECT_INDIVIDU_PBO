0d0de1afc85a8c82efcff170ad8ab963902cf708
cutfarah <cutfarahsalsabila76@gmail.com>
2025-12-03 19:57:08 +0700

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -62,3 +62,22 @@ public class Bus {
         System.out.println("Tempat sudah penuh.");
         return false;
     }
+
+       // Menghapus penumpang berdasarkan nama
+    public boolean turunkanPenumpang(String nama) {
+        return hapusDariArray(penumpangBiasa, nama) ||
+               hapusDariArray(penumpangPrioritas, nama) ||
+               hapusDariArray(penumpangBerdiri, nama);
+    }
+
+    // Menghapus penumpang dari sebuah array
+    private boolean hapusDariArray(Penumpang[] arr, String nama) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null && arr[i].getNama().equalsIgnoreCase(nama)) {
+                arr[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
